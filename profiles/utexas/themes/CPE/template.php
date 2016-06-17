219ac954f51ab6f81fd2de202dc7eee294b86583
Lacy, Lori D <l.lacy@austin.utexas.edu>
2016-06-17 14:12:50 -0500

    creation of pinterest social media icon account in template file

--- profiles/utexas/themes/CPE/template.php
+++ profiles/utexas/themes/CPE/template.php
@@ -3,3 +3,19 @@
  * @file
  * Main template file for subtheme of forty_acres theme.
  */
+
+
+function social_links_custom_field_widget_utexas_social_links_form_alter(&$element, &$form_state, $context) {
+  $links = unserialize($element['#entity']->field_utexas_social_links['und'][0]['links']);
+  // The "pinterest-square" element is used to refer to the UTexas font class "icon-pinterest-square".
+  $element['links']['vimeo-square'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Pinterest'),
+    '#size' => 60,
+    '#maxlength' => 255,
+    '#default_value' => (isset($links['pinterest-square'])) ? $links['pinterest-square'] : '',
+    '#attributes' => array(
+      'placeholder' => 'http://pinterest.com/[account_handle]',
+    ),
+  );
+}
