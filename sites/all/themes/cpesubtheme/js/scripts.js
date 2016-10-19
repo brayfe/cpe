67557060503c35fa2c5e139fc92d8858300ca5af
Rick Hastie <rhastie@austin.utexas.edu>
2016-10-19 16:33:06 -0500

    add subtheme called cpesubtheme and put in sites/all/themes

---
+++ sites/all/themes/cpesubtheme/js/scripts.js
@@ -0,0 +1,11 @@
+(function ($, Drupal, window, document, undefined) {
+
+Drupal.behaviors.utexasCustomBehavior = {
+  attach: function(context, settings) {
+
+  	//  enter custom javascript here
+
+  }
+};
+
+})(jQuery, Drupal, this, this.document);
