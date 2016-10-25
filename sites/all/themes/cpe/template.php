7a46fd5c4931f2c321d374fd747eeef687a3e0c5
Rick Hastie <rhastie@austin.utexas.edu>
2016-10-25 15:02:13 -0500

    add page tpl for instructor content type and use theme_hook_suggestions in template.php and changed the wording in the cpe theme info file; also added the page.tpl.php in the cpe theme

--- sites/all/themes/cpe/template.php
+++ sites/all/themes/cpe/template.php
@@ -3,3 +3,14 @@
  * @file
  * Main template file for subtheme of forty_acres theme.
  */
+
+/**
+ * Setup custom page templates based on suggestions.
+ */
+
+function cpe_preprocess_page(&$variables) {
+    if (isset($variables['node']->type)) {
+        $nodetype = $variables['node']->type;
+        $variables['theme_hook_suggestions'][] = 'page__' . $nodetype;
+    }
+}
