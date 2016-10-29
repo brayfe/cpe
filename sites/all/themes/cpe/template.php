b1f246a05fdec60945c04e9de695a40eaa266447
Rick Hastie <rhastie@austin.utexas.edu>
2016-10-29 18:07:18 -0500

    add the template.php so the field variables are defined

--- sites/all/themes/cpe/template.php
+++ sites/all/themes/cpe/template.php
@@ -24,6 +24,32 @@ function cpe_preprocess_page(&$variables) {
 
     		$variables['field_instructor_pic'] = field_view_field('node', $node, 'field_instructor_pic', array('settings' => array('image_style' => 'large')));
 	}
+
+   // Area of study content type field variables
+    if ($node->type == 'cpe_area_of_study') {
+
+            $variables['field_aos_headline'] = field_view_field('node', $node, 'field_aos_headline');
+
+            $variables['field_aos_description'] = field_view_field('node', $node, 'field_aos_description');
+
+            $variables['field_aos_cert_options'] = field_view_field('node', $node, 'field_aos_cert_options');
+
+            $variables['field_aos_certify_body'] = field_view_field('node', $node, 'field_aos_certify_body');
+
+            $variables['field_aos_accred_body'] = field_view_field('node', $node, 'field_aos_accred_body');
+
+            $variables['field_aos_featured'] = field_view_field('node', $node, 'field_aos_featured');
+
+            $variables['field_aos_contact_name'] = field_view_field('node', $node, 'field_aos_contact_name');
+
+            $variables['field_aos_rfi'] = field_view_field('node', $node, 'field_aos_rfi');
+
+            $variables['field_aos_contact_phone'] = field_view_field('node', $node, 'field_aos_contact_phone');
+
+            $variables['field_aos_contact_email'] = field_view_field('node', $node, 'field_aos_contact_email');
+
+    }
+
 }
 
 
