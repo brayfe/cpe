b998de3e132d6a7749ad75af0f76c6f980fcb237
Rick Hastie <rhastie@austin.utexas.edu>
2016-11-03 13:14:58 -0500

    created module for the cart and put in js files, tpl and info and .module files

---
+++ sites/all/modules/custom/cpe_cart/js/checkout_logic.js
@@ -0,0 +1,50 @@
+(function($){
+    Drupal.behaviors.modulename = {
+      attach: function (context, settings) {
+              /*Code goes here, this is going to be like 
+              $(document).ready() but will not be lost if
+              Drupal reloads the page or something */
+
+ //<![CDATA[
+ var mycookie = document.cookie;
+ function getCookie(name) {
+  var index = mycookie.indexOf(name + '=');
+  if (index == -1)
+    return null;
+  index = mycookie.indexOf("=", index) + 1;
+  var endstr = mycookie.indexOf(';', index);
+  if (endstr == -1)
+    endstr = mycookie.length;
+  return unescape(mycookie.substring(index, endstr));
+}
+
+    //var cookievalue = getCookie('class');
+    var cookievalue = '';
+    alert(cookievalue);
+    var numofcookies = 0;
+    if (cookievalue != null) {
+      var numofcookies = cookievalue.length / 22;
+    }
+
+     if (numofcookies == 1) {
+      $('#classes_cart').append(" " + numofcookies + " Item");
+    //   document.write(" " + numofcookies + " Item");
+     }
+     else
+     {
+      $('#classes_cart').append(" " + numofcookies + " Items");
+     }
+    if (numofcookies != 0) {
+
+      $('#classes_cart').append('<br /><form action="https://utdirect.utexas.edu/nlogon/cee/uex/reg1.WBX" method="get"><input type="hidden" /><input type="submit" value="Check Out >" /></form>');
+      
+      //document.write('<input type="button" value="Empty Cart" onClick="setCookie('class', '');window.location.reload();" />');
+
+    }
+
+    //]]>
+
+        }
+    }
+}(jQuery));
+   
---
+++ sites/all/modules/custom/cpe_cart/js/cpe_cart_cookie.js
@@ -0,0 +1,42 @@
+
+//<![CDATA[
+var mycookie = document.cookie;
+function getCookie(name) {
+    var index = mycookie.indexOf(name + '=');
+    if (index == -1)
+    return null;
+    index = mycookie.indexOf("=", index) + 1;
+    var endstr = mycookie.indexOf(';', index);
+    if (endstr == -1)
+    endstr = mycookie.length;
+    return unescape(mycookie.substring(index, endstr));
+}
+
+function setCookie(name, value) {
+    if (value == '') {
+        document.cookie=name + "=; path=/; domain=cpe-local.utexas.edu";
+    } else {
+        var oldvalue = getCookie(name);
+        if (oldvalue != null) {
+            var index = oldvalue.indexOf(value);
+        } else {
+            var index = -1;
+        }
+
+        if (index == -1) {
+            if (value != null && value != "") {
+                if (oldvalue != null) {
+                    document.cookie=name + "=" + oldvalue + escape(value) + "; path=/ ; domain=cpe-local.utexas.edu";
+                } else {
+                        document.cookie=name + "=" + escape(value) + "; path=/ ; domain=cpe-local.utexas.edu";
+                }
+                mycookie = document.cookie;
+            }
+            mycookie = document.cookie;
+            this.document.cartform.submit();
+        } else {
+            alert("Cart contains this course already!");
+        }
+    }
+}
+//]]>
