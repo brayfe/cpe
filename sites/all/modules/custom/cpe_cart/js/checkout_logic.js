62cdf0e44a5ead68b9ea8401ca774250420f9778
Rick Hastie <rhastie@austin.utexas.edu>
2016-11-04 11:02:47 -0500

    create cpe_cart module with js files and tpl and info files to move cart functionality into a module

--- sites/all/modules/custom/cpe_cart/js/checkout_logic.js
+++ sites/all/modules/custom/cpe_cart/js/checkout_logic.js
@@ -5,53 +5,50 @@
               $(document).ready() but will not be lost if
               Drupal reloads the page or something */
 
- //<![CDATA[
- var mycookie = document.cookie;
+     //<![CDATA[
+      var mycookie = document.cookie;
 
- $('#empty_cart').on('click',function() {
-   setCookie('class', '');
-   alert("hi");
-   location.reload();
- });
+      function getCookie(name) {
+        var index = mycookie.indexOf(name + '=');
+        if (index == -1)
+          return null;
+          index = mycookie.indexOf("=", index) + 1;
+          var endstr = mycookie.indexOf(';', index);
+        if (endstr == -1)
+          endstr = mycookie.length;
+          return unescape(mycookie.substring(index, endstr));
+      }
 
- function getCookie(name) {
-  var index = mycookie.indexOf(name + '=');
-  if (index == -1)
-    return null;
-  index = mycookie.indexOf("=", index) + 1;
-  var endstr = mycookie.indexOf(';', index);
-  if (endstr == -1)
-    endstr = mycookie.length;
-  return unescape(mycookie.substring(index, endstr));
-}
+      var cookievalue = getCookie('class');
+      var numofcookies = 0;
+      if (cookievalue != null) {
+        var numofcookies = cookievalue.length / 22;
+      }
 
-    var cookievalue = getCookie('class');
-    alert(cookievalue);
-    var numofcookies = 0;
-    if (cookievalue != null) {
-      var numofcookies = cookievalue.length / 22;
+      if (numofcookies == 1) {
+        $('#classes_cart').append(" " + numofcookies + " Item");
+      }
+      else
+      {
+        $('#classes_cart').append(" " + numofcookies + " Items");
+      }
+
+      if (numofcookies != 0) {
+
+        $('#classes_cart').append('<br /><form action="' + Drupal.settings.cpe_cart.cpeCartCheckoutUrl + '" method="get"><input type="hidden" /><input type="submit" value="Check Out >" /></form>');
+
+        $('#classes_cart').append('<input id="empty_cart" type="button" value="Empty Cart"  />');
+        
+        $('#empty_cart').on('click',function() {
+          setCookie('class', '');
+          location.reload();
+        });
+
+      }
+
+      //]]>
+
     }
-
-    if (numofcookies == 1) {
-      $('#classes_cart').append(" " + numofcookies + " Item");
-      //   document.write(" " + numofcookies + " Item");
-    }
-    else
-    {
-      $('#classes_cart').append(" " + numofcookies + " Items");
-    }
-
-    if (numofcookies != 0) {
-
-      $('#classes_cart').append('<br /><form action="' + Drupal.settings.cpe_cart.cpeCartCheckoutUrl + '" method="get"><input type="hidden" /><input type="submit" value="Check Out >" /></form>');
-
-      $('#classes_cart').append('<input id="empty_cart" type="button" value="Empty Cart"  />');
-      //onClick="setCookie('class', '');window.location.reload();"
-    }
-
-    //]]>
-
   }
-}
 }(jQuery));
 
