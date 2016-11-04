9264a9a5146bdd6daa6ad0a0a73e925f83cd68eb
Francine Bray <fbray@austin.utexas.edu>
2016-11-04 17:26:45 -0500

    Updated field tpl per client request.

--- sites/all/modules/custom/cpe_continuing_ed_hrs/theme/cont-ed-hrs.tpl.php
+++ sites/all/modules/custom/cpe_continuing_ed_hrs/theme/cont-ed-hrs.tpl.php
@@ -5,16 +5,7 @@
  */
 ?>
 <div id="cont-ed-field">
-  <div id="accrediting-body">
-    <div class="field-label">Accrediting Body: </div>
-    <div class="field-items"><?= $ac_body; ?></div>
-  </div>
-  <div id="prof-unit-type">
-    <div class="field-label">Professional Unit Type: </div>
-    <div class="field-items"><?= $put; ?></div>
-  </div>
-  <div id="hours">
-    <div class="field-label">Number of Hours: </div>
-    <div class="field-items"><?= $num_hours; ?></div>
-  </div>
+  <span class="field-items"><?= $ac_body; ?> </span>
+  <span class="field-items"><?= $num_hours; ?> </span>
+  <span class="field-items"><?= $put; ?></span>
 </div>
