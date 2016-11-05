3d7caf5bb54291752260277a7a32097e1014038c
Francine Bray <fbray@austin.utexas.edu>
2016-11-05 08:54:56 -0500

    Removed php short tags from CEH template.

--- sites/all/modules/custom/cpe_continuing_ed_hrs/theme/cont-ed-hrs.tpl.php
+++ sites/all/modules/custom/cpe_continuing_ed_hrs/theme/cont-ed-hrs.tpl.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div id="cont-ed-field">
-  <span class="field-items"><?= $ac_body; ?> </span>
-  <span class="field-items"><?= $num_hours; ?> </span>
-  <span class="field-items"><?= $put; ?></span>
+  <span class="field-items"><?php print $ac_body; ?> </span>
+  <span class="field-items"><?php print $num_hours; ?> </span>
+  <span class="field-items"><?php print $put; ?></span>
 </div>
