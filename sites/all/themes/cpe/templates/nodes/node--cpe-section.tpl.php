364c5535124e3c31c2f8562554ae23feb305dc5f
Rick Hastie <rhastie@austin.utexas.edu>
2016-11-07 21:40:06 -0600

    add button code in section tpl and comment out set cookie code in cpe_cart module

--- sites/all/themes/cpe/templates/nodes/node--cpe-section.tpl.php
+++ sites/all/themes/cpe/templates/nodes/node--cpe-section.tpl.php
@@ -121,6 +121,11 @@
       print render($content['field_section_course_id']);
     ?>
 
+    <input type="submit" value="Add to Cart" id="Adding PDC course" setcookie('class','0UE14173rS000000000000', time() + 3600, '/', 'utexas.edu'); />
+
+    <input type="submit" value="Add to Cart" id="Contact Coordinator" />
+    
+
     <!-- hide mishell field for now; this will be used later with the add to cart button-->
     <?php //print $field_section_mishell_id; ?>
 
