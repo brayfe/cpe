6c9447431b2b36409758c3bbc879a79237458b31
Paul Grotevant <pfg@utexas.edu>
2016-11-11 17:30:49 -0600

    Added docblock to seat check wrapper.

--- sites/all/modules/custom/cpe_cart/seat_check.php
+++ sites/all/modules/custom/cpe_cart/seat_check.php
@@ -1,8 +1,20 @@
 <?php
 
+/**
+ * @file
+ * Wrapper script for querying MISHELL.
+ *
+ * This is a "wrapper" script for querying MISHELL on UT Direct
+ * to check for seat availability on course sections in the Center
+ * for Professional Education. The only reason it has to be done`
+ * this way is to avoid a CORS violation when making an AJAX request
+ * to check seat availability.
+ */
+
 if (preg_match('/^[a-zA-Z0-9_]+$/', $_GET['s_pgm_id'])) {
   $s_pgm_id = $_GET['s_pgm_id'];
-} else {
+}
+else {
   print 'INVALID INPUT';
   exit();
 }
