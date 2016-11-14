ede6485b7780e1c4052c33ee0eb33335957bab29
Paul Grotevant <pfg@utexas.edu>
2016-11-13 18:04:29 -0600

    Refactor the MISHELL check callback, based on markup changes and being able to test with multiple sections.

--- sites/all/modules/custom/cpe_cart/js/checkout_logic.js
+++ sites/all/modules/custom/cpe_cart/js/checkout_logic.js
@@ -1,7 +1,13 @@
-(function ($) {
+/**
+ * @file
+ * Attaches the behaviors for the CPE cart checkout.
+ */
+
+ (function ($) {
   Drupal.behaviors.modulename = {
     attach: function (context, settings) {
 
+      // Attach a click behavior to the "add to cart" button.
       $('input#add-to-cart').on('click', function () {
         var classId = $(this).data('class-id');
         setCookie('class', classId);
@@ -9,34 +15,45 @@
       });
 
       // Hide the "Add to Cart" button if MISHELL returns a response for
-      // the seat check that isn't a number greater than zero.
-      $('div.node-cpe-section').each(function() {
+      // the seat check that isn't a number greater than zero. If the response
+      // is between 1-4, show the number of seats remaining as a sepaarate
+      // field on the section.
+      $('#block-views-cpe-sections-block div.node-cpe-section').each(function () {
         if ($('input#add-to-cart', this).length) {
           var $addToCartButton = $('input#add-to-cart', this);
-          var $emailCoordinatorButton = $('input#email-coord', this);
           var classId = $addToCartButton.data('class-id');
-          $.get(Drupal.settings.cpe_cart.cpeCartSeatCheckUrl + classId, function(data) {
-            if (data > 0) {
-              // Hide the "Contact Coodinator" button if there are seats available
+          var $emailCoordinatorButton = $('button#email-coord', this);
+          $('div.field_section_course_id', this).before('<div class="field field_seats_remaining"><div class="field-label">Seats Remaining: </div><div class="field-items"></div></div></div>');
+          var $seatsRemainingField = $('div.field_seats_remaining', this);
+          $seatsRemainingField.hide();
+          $.get(Drupal.settings.cpe_cart.cpeCartSeatCheckUrl + classId, function (data) {
+            var seatsAvailable = parseInt(data);
+            $seatsRemainingField.children('.field-items').text(seatsAvailable);
+            if (seatsAvailable > 0) {
+              // Hide the "Contact Coodinator" button if there are seats available.
               $emailCoordinatorButton.hide();
-              if (data < 5) {
-                // Add a pseudo "Seats Remaining" field if there are 1-4 seats available
-                $('div.field_section_course_id').before('<div class="field field_seats_remaining"><div class="field-label">Seats Remaining: </div><div class="field-items">' + data + '</div></div></div>');
+              if (seatsAvailable < 5) {
+                // Show the "Seats Remaining" field if there are 1-4 seats available.
+                $seatsRemainingField.show();
               }
-            } else {
-              // Hide the "Add to Cart" button if there are no seats available
-              $addToCartButton.hide()
+            }
+            else {
+              // Hide the "Add to Cart" button if there are no seats available.
+              $addToCartButton.hide();
             }
           });
         }
       });
 
+      // Parse the "class" cookie and divide the contents by the length of the
+      // classId as determined by the cpe_cart_class_id_length $conf variable.
       var cookieVal = getCookie('class');
       var numOfClasses = 0;
       if (cookieVal != null) {
         var numOfClasses = cookieVal.length / Drupal.settings.cpe_cart.cpeCartClassIdLength;
       }
 
+      // Change the text of the number of classes in the cart based on singular or plural.
       if (numOfClasses == 1) {
         $('#classes_cart').append(" " + numOfClasses + " Item");
       }
@@ -44,11 +61,14 @@
         $('#classes_cart').append(" " + numOfClasses + " Items");
       }
 
+      // If there are classes in the cart, change the available actions.
       if (numOfClasses > 0) {
         $('#classes_cart').append('<br /><form action="' + Drupal.settings.cpe_cart.cpeCartCheckoutUrl + '" method="get"><input type="submit" value="Check Out >" /></form>');
 
         $('#classes_cart').append('<input id="empty_cart" type="button" value="Empty Cart"  />');
 
+        // Add the click behavior for the "empty cart" button to reset the
+        // "class" cookie and reload the page.
         $('#empty_cart').on('click', function () {
           setCookie('class', '');
           location.reload();
