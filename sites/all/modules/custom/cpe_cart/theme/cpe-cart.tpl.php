7a1e3aaffbb9522a075b1577a0a223de4fbf6eda
Rick Hastie <rhastie@austin.utexas.edu>
2016-11-22 09:25:29 -0600

    add new cart icon to images dir incpe theme; change path in cpe_cart/theme/cpe-cart.tpl.php

--- sites/all/modules/custom/cpe_cart/theme/cpe-cart.tpl.php
+++ sites/all/modules/custom/cpe_cart/theme/cpe-cart.tpl.php
@@ -7,6 +7,6 @@
 ?>
 
 <div id="classes_cart" class="shopping-cart">
-  <img src="http://extendedcampus.utexas.edu/img/general/cart.png" width="30" height="30" alt="Shopping Cart Image" />
+  <img src="sites/all/themes/cpe/images/cart.png" width="30" height="30" alt="Shopping Cart Image" />
   <form name="cartform" id="cartform" action="" method="get"></form>
 </div>
